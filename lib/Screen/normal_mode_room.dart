3f91f12ed88428c07e2d850db62d30d1f7a55a53
zont09 <103422407+zont09@users.noreply.github.com>
2024-05-24 20:18:38 +0700

    Revert "Thinh"

--- lib/Screen/normal_mode_room.dart
+++ lib/Screen/normal_mode_room.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:draw_and_guess_promax/Widget/Drawing.dart';
-import 'package:draw_and_guess_promax/Widget/ChatWidget.dart';
 
 class NormalModeRoom extends StatefulWidget {
   NormalModeRoom({
@@ -15,17 +14,10 @@ class NormalModeRoom extends StatefulWidget {
 class _NormalModeRoomState extends State<NormalModeRoom> {
   // MediaQuery.of(context).size.height
   late String _wordToDraw;
-  late bool _isOpenChat = false;
   @override
   void initState() {
     super.initState();
     _wordToDraw = "Con ga";
-    _isOpenChat = false;
-  }
-  void _openChat() {
-    setState(() {
-      _isOpenChat = !_isOpenChat;
-    });
   }
   @override
   Widget build(BuildContext context) {
@@ -39,45 +31,12 @@ class _NormalModeRoomState extends State<NormalModeRoom> {
         ),
         title: Text(_wordToDraw),
         backgroundColor: Color(0xFF00C4A1),
-        actions: [
-          // Add your desired icon button here
-          IconButton(
-            icon: Icon(Icons.chat, color: Colors.black,), // Replace with your icon
-            onPressed: () {
-              _openChat();
-            },
-          ),
-        ],
       ),
       body: LayoutBuilder(
         builder: (context, constraints) {
-          return Stack(
-            children: [
-              Positioned(
-                  top: 0,
-                  left: 0,
-                  child: Container(
-                    height: constraints.maxHeight,
-                    width: constraints.maxWidth,
-                    child: Drawing(
-                      height: constraints.maxHeight,
-                      width: constraints.maxWidth,
-                    ),
-                  )
-              ),
-              AnimatedPositioned(
-                bottom: _isOpenChat ? 0 : -constraints.maxHeight*2/3,
-                left: 0.0,
-                duration: const Duration(milliseconds: 300), // Adjust animation duration
-                curve: Curves.easeInCubic, // Customize animation curve
-                child: Container(
-                  height: constraints.maxHeight*2/3,
-                  width: constraints.maxWidth,
-                  color: Colors.white, // Or your preferred chat background color
-                  child: Chat(roomId: widget.roomId, height: constraints.maxHeight*2/3, width: constraints.maxWidth),
-                ),
-              ),
-            ],
+          return Drawing(
+            height: constraints.maxHeight,
+            width: constraints.maxWidth,
           );
         },
       ),
--- lib/Screen/waiting_room.dart
+++ lib/Screen/waiting_room.dart
@@ -1,10 +1,5 @@
-<<<<<<< Thinh
-import 'package:draw_and_guess_promax/Screen/normal_mode_room.dart';
-import 'package:draw_and_guess_promax/Widget/Drawing.dart';
-=======
 import 'dart:async';
 
->>>>>>> main
 import 'package:draw_and_guess_promax/Widget/player.dart';
 import 'package:draw_and_guess_promax/Widget/room_mode.dart';
 import 'package:draw_and_guess_promax/data/play_mode_data.dart';
@@ -28,9 +23,6 @@ class WaitingRoom extends ConsumerStatefulWidget {
   final Room selectedRoom;
   final bool isGuest;
 
-<<<<<<< Thinh
-  void _startClick(context) {
-=======
   @override
   ConsumerState<WaitingRoom> createState() => _WaitingRoomState();
 }
@@ -148,10 +140,7 @@ class _WaitingRoomState extends ConsumerState<WaitingRoom> {
   }
 
   void _startClick() {
->>>>>>> main
     print('bắt đầu');
-    Navigator.of(context).pushReplacement(MaterialPageRoute(
-        builder: (ctx) => NormalModeRoom(roomId: selectedRoom.roomId)));
   }
 
   void _inviteClick() {
@@ -292,12 +281,7 @@ class _WaitingRoomState extends ConsumerState<WaitingRoom> {
                     itemBuilder: (BuildContext context, int index) {
                       // Tạo một avatar từ index
                       return Player(
-<<<<<<< Thinh
-                        player: availablePlayer[index],
-                        sizeimg: 100,
-=======
                         player: currentPlayers[index],
->>>>>>> main
                       );
                     },
                   ),
@@ -344,7 +328,7 @@ class _WaitingRoomState extends ConsumerState<WaitingRoom> {
                   const SizedBox(width: 10),
                   Button(
                     onClick: (ctx) {
-                      _startClick(ctx);
+                      _startClick();
                     },
                     title: 'Bắt đầu',
                     imageAsset: 'assets/images/play.png',
