b01fada7aeadc2cbbc52cc58a6b9d36e6165ed27
zont09 <zontcn09@gmail.com>
2024-05-24 20:45:04 +0700

    them chat

--- lib/Widget/player.dart
+++ lib/Widget/player.dart
@@ -9,15 +9,16 @@ class Player extends StatelessWidget {
   const Player({
     super.key,
     required this.player,
+    required this.sizeimg,
   });
-
+  final double sizeimg;
   final User player;
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: 100,
-      height: 100,
+      width: sizeimg,
+      height: sizeimg,
       child: Stack(
         children: [
           Positioned(
